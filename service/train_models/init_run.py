7ab55369c5380a3742a1eb751735a45bd3cf472a
akhmadiev <akhmadiev@legaldoc.ai>
2023-10-18 00:34:09 +0400

    feat: init yolo8 train

--- service/train_models/init_run.py
+++ service/train_models/init_run.py
@@ -6,10 +6,10 @@ from ultralytics import YOLO, settings
 # Settings init
 ROOT = Path().cwd()
 
-DATASET = 'dentasis_v32i_Caries_v0-v0'
+DATASET = 'xd_n_v1i_Caries_v0'
 DATA = ROOT / 'service' / 'data' / DATASET
 
-wandb.login(key='')
+wandb.login(key='a427b2ba60f438b85345fe30f56d9f8fd6802294')
 run = wandb.init(
     entity="dentist_ai",
     project="Dentist_AI",
@@ -26,13 +26,13 @@ model = YOLO('yolov8m.yaml')
 if DATA.is_dir():
     artifact_dir = DATA
 else:
-    artifact = run.use_artifact("dentist_ai/Dentist_AI/dentasis_v32i_Caries_v0:v0", type="dataset")
+    artifact = run.use_artifact("dentist_ai/Dentist_AI/xd_n.v1i_Caries_v0:v0", type="dataset")
     artifact.download(DATA)
 
 # Train the model using the 'coco128.yaml' dataset for 3 epochs
 model.train(data=DATA / 'data.yaml',
             task='detect',
-            epochs=30)
+            epochs=1)
 
 # Evaluate the model's performance on the validation set
 results = model.val()
@@ -41,12 +41,12 @@ print(results)
 print("----------------------------- ----------- ----------------------------")
 
 # Export the model to ONNX format
-# success = model.export(format='onnx')
+success = model.export(format='onnx')
 
 # Perform object detection on an image using the model
-results = model(DATA / 'valid' / 'images' / '1_jpeg.rf.6e6037fef3b0f8023be0f9a9d715980b.jpg')
-print("----------------------------- Prediction ----------------------------")
-print(results)
+# results = model(DATA / 'valid' / 'images' / '1_jpeg.rf.6e6037fef3b0f8023be0f9a9d715980b.jpg')
+# print("----------------------------- Prediction ----------------------------")
+# print(results)
 
 # Reset settings to default values
 # settings.reset()
